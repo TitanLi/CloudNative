d7f3fead733e1523de41ea73c5681a16c9683dd9
TitanLi <lisheng0706@gmail.com>
2020-05-18 18:21:41 +0800

    Update reinstall-node.sh

--- kubernetes/reinstall-node.sh
+++ kubernetes/reinstall-node.sh
@@ -36,3 +36,20 @@ apt-get install -y kubelet kubeadm kubectl
 swapoff -a
 
 echo y | kubeadm reset
+
+systemctl stop kubelet
+systemctl stop docker
+
+rm -rf /var/lib/cni/
+rm -rf /var/lib/kubelet/*
+rm -rf /etc/cni/
+
+ifconfig cni0 down
+ifconfig flannel.1 down
+ifconfig docker0 down
+
+ip link delete cni0
+ip link delete flannel.1
+
+systemctl start docker
+systemctl start kubelet
